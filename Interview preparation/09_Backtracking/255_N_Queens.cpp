4166b96a595f56adc5900e16655e791870c67635
sasidharan10 <ksasidharan98@gmail.com>
2021-12-06 12:00:04 +0530

    commit

---
+++ Interview preparation/09_Backtracking/255_N_Queens.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+bool isSafe(int row, int col, vector<vector<int>> &board, int n)
+{
+    // queen can go in 8 directions, since we are going from left to right there will not be any queens in the
+    // right side of the spot where th contol is or col is,and as there would be only 1 queen in a
+    // column,therefore top and bootom is also not needed. So we only need to check 3 sides on left;
+
+    int dumRow = row;
+    int dumCol = col;
+    // left upper cross
+    while (row >= 0 && col >= 0)
+    {
+        if (board[row][col] == 1)
+            return false;
+        row--;
+        col--;
+    }
+    row = dumRow;
+    col = dumCol;
+    // left
+    while (col >= 0)
+    {
+        if (board[row][col] == 1)
+            return false;
+        col--;
+    }
+    row = dumRow;
+    col = dumCol;
+    // left down cross
+    while (row < n && col >= 0)
+    {
+        if (board[row][col] == 1)
+            return false;
+        row++;
+        col--;
+    }
+    return true;
+}
+void solve(int col, int n, vector<vector<int>> &board)
+{
+    if (col == n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                cout << board[i][j] << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+        return;
+        true;
+    }
+    for (int row = 0; row < n; row++)
+    {
+        if (isSafe(row, col, board, n))
+        {
+            board[row][col] = 1;
+            solve(col + 1, n, board);
+            board[row][col] = 0;
+        }
+    }
+}
+void solve2(int col, int n, vector<vector<int>> &board, vector<int> &leftRow, vector<int> &leftUp,
+            vector<int> &leftDown)
+{
+    if (col == n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                cout << board[i][j] << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+        return;
+    }
+    for (int row = 0; row < n; row++)
+    {
+        if (leftRow[row] == 0 && leftUp[row + col] == 0 && leftDown[(n - 1) + (col - row)] == 0)
+        {
+            board[row][col] = 1; // marking queen
+            leftRow[row] = 1;    // marking row as it has queen now
+            leftUp[row + col] = 1;
+            leftDown[(n - 1) + (col - row)] = 1;
+            solve2(col + 1, n, board, leftRow, leftUp, leftDown);
+            board[row][col] = 0;
+            leftRow[row] = 0;
+            leftUp[row + col] = 0;
+            leftDown[(n - 1) + (col - row)] = 0;
+        }
+    }
+    return;
+}
+void nQueen(int n)
+{
+    vector<vector<int>> board(n, vector<int>(n, 0));
+    // solve(0, n, board);
+
+    // optimised technique
+
+    vector<int> leftRow(n, 0), leftUp(2 * n - 1, 0), leftDown(2 * n - 1, 0);
+    solve2(0, n, board, leftRow, leftUp, leftDown);
+}
+
+int main()
+{
+    int n = 4;
+    nQueen(n);
+    return 0;
+}
--- basic c++ problems/aa_practice.cpp
+++ basic c++ problems/aa_practice.cpp
@@ -1,40 +1,81 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
 using namespace std;
-void assemblyLine(int A[],int N)
-{
-    int result=-404;
-    result=0;
-    int b[N];
-    for (int i = 0; i < N; i++)
-    {
-        b[i]=A[i];
+int n;
+
+bool isSafe(int row,int col, vector<vector<int>> &mat){
+    //queen can go in 8 directions, since we are going from left to right there will not be any queens in the
+    //right side of the spot where th contol is or col is,and as there would be only 1 queen in a
+    //column,therefore top and bootom is also not needed. So we only need to check 3 sides on left;
+
+    //row--col--
+    for(int i=row,j=col;i>=0&&j>=0;i--,j--){
+        if(mat[i][j]==1) return false;
     }
-    int f=0;
-    sort(b,b+N);
-    for (int i = 0; i < N; i++)
-    {
-        if(A[i]!=b[i])
-        {
-            cout<<i<<" ";
-            f=1;
+    //row col--
+    for(int i=row,j=col;i>=0&&j>=0;j--){
+        if(mat[i][j]==1) return false;
+    }
+    //row++col--
+    for(int i=row,j=col;i<n&&j>=0;i++,j--){
+        if(mat[i][j]==1) return false;
+    }
+    //if it never failed then the position is definently safe
+    return true;
+    
+}
+//to print the first possible way
+// bool nqueens(int col,vector<vector<int>>&mat){
+//     if(col==n){
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<n;j++)
+//                 cout<<mat[i][j]<<" ";
+//                 cout<<endl;
+//         }
+//         return true;
+//     }
+//     for(int row=0;row<n;row++){
+//         if(isSafe(row,col,mat)){
+//             mat[row][col]=1;
+//             if(nqueens(col+1,mat)==true){
+//                 return true;
+//             }
+//             mat[row][col]=0;
+//         }
+//     }
+//     return false;
+// }
+
+
+// to print all the posiible ways
+void nqueens(int col,vector<vector<int>>&mat){
+    if(col==n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++)
+                cout<<mat[i][j]<<" ";
+                cout<<endl;
+        }
+        cout<<endl<<endl;
+        return;
+    }
+    for(int row=0;row<n;row++){
+        if(isSafe(row,col,mat)){
+            mat[row][col]=1;
+            nqueens(col+1,mat);
+            // if(nqueens(col+1,mat)==true){
+            //     return true;
+            // }
+            mat[row][col]=0;
         }
     }
-    if(f==0)
-    {
-        cout<<result;
-    }
-
+    
 }
-int main()
-{
-    int N,i;
-    cin>>N;
-    int A[N];
-    for (int i = 0; i < N; i++)
-    {
-        cin>>A[i];
-    }
-    assemblyLine(A,N);
-    return 0;
+
+int main(){
+    // cin>>n;
+    n=4;
+    vector<vector<int>>mat(n,vector<int>(n,0));
+    nqueens(0,mat);
 }
