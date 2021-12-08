77724f37dc3397bac20d23939541a5e349462149
sasidharan10 <ksasidharan98@gmail.com>
2021-12-08 19:02:58 +0530

    commit

--- basic c++ problems/aa_practice.cpp
+++ basic c++ problems/aa_practice.cpp
@@ -1,81 +1,45 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <string>
+#include<iostream>
+#include<vector>
 using namespace std;
-int n;
 
-bool isSafe(int row,int col, vector<vector<int>> &mat){
-    //queen can go in 8 directions, since we are going from left to right there will not be any queens in the
-    //right side of the spot where th contol is or col is,and as there would be only 1 queen in a
-    //column,therefore top and bootom is also not needed. So we only need to check 3 sides on left;
-
-    //row--col--
-    for(int i=row,j=col;i>=0&&j>=0;i--,j--){
-        if(mat[i][j]==1) return false;
+void merge(vector<int> &arr,int l,int mid,int r){
+    int n1=mid-l+1;
+    int n2=r-mid;
+    int *left=new int[n1];
+    int *right=new int[n2];
+    for(int i=0;i<n1;i++)
+        left[i]=arr[l+i];
+    for(int i=0;i<n2;i++)
+        right[i]=arr[mid+i+1];
+    int i=0,j=0,k=l;
+    while(i<n1&&j<n2){
+        if(left[i]<=right[j])
+            arr[k++]=left[i++];
+        else
+            arr[k++]=right[j++];
     }
-    //row col--
-    for(int i=row,j=col;i>=0&&j>=0;j--){
-        if(mat[i][j]==1) return false;
-    }
-    //row++col--
-    for(int i=row,j=col;i<n&&j>=0;i++,j--){
-        if(mat[i][j]==1) return false;
-    }
-    //if it never failed then the position is definently safe
-    return true;
-    
+    while(i<n1)
+        arr[k++]=left[i++];
+    while(j<n2)
+        arr[k++]=right[j++];
+        delete left;
+        delete right;
 }
-//to print the first possible way
-// bool nqueens(int col,vector<vector<int>>&mat){
-//     if(col==n){
-//         for(int i=0;i<n;i++){
-//             for(int j=0;j<n;j++)
-//                 cout<<mat[i][j]<<" ";
-//                 cout<<endl;
-//         }
-//         return true;
-//     }
-//     for(int row=0;row<n;row++){
-//         if(isSafe(row,col,mat)){
-//             mat[row][col]=1;
-//             if(nqueens(col+1,mat)==true){
-//                 return true;
-//             }
-//             mat[row][col]=0;
-//         }
-//     }
-//     return false;
-// }
-
-
-// to print all the posiible ways
-void nqueens(int col,vector<vector<int>>&mat){
-    if(col==n){
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++)
-                cout<<mat[i][j]<<" ";
-                cout<<endl;
-        }
-        cout<<endl<<endl;
-        return;
-    }
-    for(int row=0;row<n;row++){
-        if(isSafe(row,col,mat)){
-            mat[row][col]=1;
-            nqueens(col+1,mat);
-            // if(nqueens(col+1,mat)==true){
-            //     return true;
-            // }
-            mat[row][col]=0;
-        }
-    }
-    
+void mergesort(vector<int> &arr,int l,int r){
+    if(l>=r) return ;
+    int mid=l+(r-l)/2;
+    mergesort(arr,l,mid);
+    mergesort(arr,mid+1,r);
+    merge(arr,l,mid,r);
 }
-
 int main(){
-    // cin>>n;
-    n=4;
-    vector<vector<int>>mat(n,vector<int>(n,0));
-    nqueens(0,mat);
+    int a[] = {6, 5, 8, 4, 7, 2, 3, 1,9};
+    int n = sizeof(a) / sizeof(a[0]);
+    vector<int> arr(a,a+n);
+    mergesort(arr,0,n-1);
+    for (int i = 0; i < n; i++)
+    {       
+        cout<<arr[i];
+    }
+    
 }
