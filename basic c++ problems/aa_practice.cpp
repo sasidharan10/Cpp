107a60a4a0bb8ef1ef1e07054feb4c4aabd37824
sasidharan10 <ksasidharan98@gmail.com>
2022-06-11 21:04:53 +0530

    code formated

--- basic c++ problems/aa_practice.cpp
+++ basic c++ problems/aa_practice.cpp
@@ -1,13 +1,30 @@
 #include<iostream>
 #include<algorithm>
-#include<vector>
+#include<unordered_set>
 using namespace std;
-int main(){
-    int a[] = {6, 5, 8, 4, 7, 2, 3, 1,9};
-    int n = sizeof(a) / sizeof(a[0]);
-    cout<<max_element(a,a+n)-a<<endl;
-    vector<int>v(a,a+n);
-    cout<<max_element(v.begin(),v.end())-v.begin()<<endl;
+void calc(int i,int n,int k,int x,int y,unordered_set<int> &s)
+{
+    int t=abs(k-i);
+    int res=(t*x-(n-t)*y) + (i*x-(n-i)*y);
+    if(s.find(res)==s.end())
+        s.insert(res);
+    return;
+}
+int main()
+{
+    int n=3,x=1,y=1,z=3;
+    int count=0;
+    int j=0;
+    unordered_set<int> s;
+    for (int i = 1,j=i+1; i <= n && j<=n; i++)
+    {
+        int k=n-i+j;
+        calc(i,n,k,x,y,s);
+        calc(n-i,n,k,x,y,s);
+    }
+    for (auto i : s)
+        cout<<i<<" ";
+    cout<<endl;
+    cout<<s.size();
     return 0;
-    
 }
--- basic c++ problems/add.cpp
+++ basic c++ problems/add.cpp
@@ -1,13 +1,13 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
 	cout << "Hello World\n";
-	int a=5,b=10,c;
-	float e=9.4;
-    c=a+b;
-    cout<<"c : "<<c<<endl;
-    cout<<"e : "<<(int)e/2<<endl;  // int always takes floor value
-	printf("e : %.2f\n", e);
-	cout <<"sizeof(long) : "<< sizeof(long);
+	int a = 5, b = 10, c;
+	float e = 9.4;
+	c = a + b;
+	cout << "c : " << c << endl;
+	cout << "e : " << (int)e / 2 << endl; // int always takes floor value
+	printf("e : %.4f\n", e);
+	cout << "sizeof(long) : " << sizeof(long);
 }
