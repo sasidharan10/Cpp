277f6d4fd5b769c3685cbbfb960eca666c26b8da
sasidharan10 <ksasidharan98@gmail.com>
2022-06-27 23:25:02 +0530

    added cpp _output code

--- basic c++ problems/cpp_output.cpp
+++ basic c++ problems/cpp_output.cpp
@@ -1,29 +1,32 @@
 #include<iostream>
-#include<cstdio>    // for fgets()
-#include<cstring>   // string.h in C lang
-#include<string>    // to use string DS
-using namespace std;
+#include<cstdio>      // for fgets()
+#include<cstring>     // string.h in C lang
+#include<string>      // to use string DS
+using namespace std;  // cin, cout, endl are members of std namespace
 int main()
 {
     int a;
-    cout<<"Enter number: "<<endl;
+    cout<<"Enter number: ";
     cin>>a;
-    cout<<"Number: "<<a<<endl;
-    // char b[30];
-    cout<<"Enter string: "<<endl;
-    string c;
-    // cin>>b;                 // cin cannot take strings with spaces
-	// gets(b);                // avoid it because it does not support buffer overflow()
-	// fgets(b,sizeof(b),stdin);  // recommended
-    // getline(cin,c);         // this fn is for string DS
-    // printf("String b: %s",b);
-    getline(cin,c);
-    cout<<"String c: "<<c<<endl;
+    cerr<<"Number: "<<a;
+    string b;
+    cout<<"\nEnter string b: ";
+    // cin.ignore();
+    getline(cin>>ws,b);        // this fn is for string DS
+    cout<<"String b: "<<b;
+    char c[30];
+    // cin>>c;                 // cin cannot take strings with spaces
+	// gets(c);                // avoid it because it does not support buffer overflow()
+    cout<<"\nEnter string c: ";
+	fgets(c,sizeof(c),stdin);  // recommended        
+    cout<<"Char array c: "<<c;
     return 0;
 }
 
 /*
 - cerr in C++ is un-buffered so it is used when one needs to display the error message immediately.
-
+- getline() wont take input after "cin" since '\n' is still in buffer and when getline() encounters
+  '\n', it stops taking input.
+- we ca use cin.ignore() or getline(cin>>ws,c) to clear that '\n' from the buffer.
 
 */
