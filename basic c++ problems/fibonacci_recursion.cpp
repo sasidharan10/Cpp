4a04dcfdfeec181eb737bd2b88e0fffc15fcfc4c
sasidharan10 <ksasidharan98@gmail.com>
2022-06-29 23:00:59 +0530

    edited code

--- basic c++ problems/fibonacci_recursion.cpp
+++ basic c++ problems/fibonacci_recursion.cpp
@@ -1,38 +1,43 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
-int fib(int x)
+int fib1(int x)
 {
     if (x <= 1)
         return x;
     else
-        return fib(x - 1) + fib(x - 2);
+        return fib1(x - 1) + fib1(x - 2);
 }
 int fib2(int x)
 {
     int a = 0;
     int b = 1;
-    int c = a+b;
-    if (x == 0 || x == 1)
+    int c = a + b;
+    if (x == 0)
         return a;
-    if (x == 2)
-        return b;
-    for (int i = 2; i < x; i++)
+    for (int i = 2; i <= x; i++)
     {
-        cout<<c<<" ";
+        // cout<<c<<" ";
         c = a + b;
         a = b;
         b = c;
     }
-    cout<<endl;
-    return b;
+    return c;
+}
+int fib3(int x)
+{
+    double phi = (sqrt(5) + 1) / 2;
+    return round(pow(phi, x) / sqrt(5));
 }
 int main()
 {
-    int x = 8;
+    int x = 9;
     cout << "fibonacci series : ";
-    cout << fib(x);
-    cout << endl;
+    cout << fib1(x);
+    cout << "\nfibonacci series (loop) : ";
     cout << fib2(x);
+    cout << "\nfibonacci series (O(logn)) : ";
+    cout << fib3(x);
 
     return 0;
 }
