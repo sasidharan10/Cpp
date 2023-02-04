9140c0d33c63bb1d38eb27c5db2640e780ce7635
sasidharan10 <ksasidharan98@gmail.com>
2023-02-05 02:37:31 +0530

    commit

--- basic c++ problems/practice.cpp
+++ basic c++ problems/practice.cpp
@@ -1,31 +1,8 @@
-#include<iostream>
-#include<algorithm>
+#include <iostream>
 using namespace std;
 int main()
 {
-
-    int a[]={3,3,3,3};
-    int n=sizeof(a)/sizeof(a[0]);
-    int k=3;
-    int count=0,sum=0;
-    int l=0,j=0;
-    for (int i = 0; i < n; i++)
-    {
-        l=k+i;
-        for (int j = i; j < l; j++)
-        {
-            sum+=a[j];
-        }
-        
-        l=0;
-        if(sum%2==0)
-            count++;
-        sum=0;
-    }
-    if(count==0)
-        count=-1;
-    return count;
-    cout<<"Count : "<<count<<endl;
+    int a = 5, b = 2;
+    cout << "Addition: " << a + b;
     return 0;
-
 }
