4bc1df8ea26be25a0142fc5ea63efe9502e68098
sasidharan10 <ksasidharan98@gmail.com>
2023-02-26 02:27:59 +0530

    c

--- basic c++ problems/sizeof.cpp
+++ basic c++ problems/sizeof.cpp
@@ -5,9 +5,9 @@ int main()
     int *p;
     int arr[]{1, 2, 3, 4, 5, 6, 7};
     p = arr;
-    cout << "<sizeof(arr): " << sizeof(arr) << endl;   // compiler knows its an array, hence returns total size
-    cout << "<sizeof(&arr): " << sizeof(&arr) << endl; // size of pointer
-    cout << "<sizeof(arr[0]): " << sizeof(arr[0]) << endl;
-    cout << "<sizeof(p): " << sizeof(p) << endl;
+    cout << "sizeof(arr): " << sizeof(arr) << endl;   // compiler knows its an array, hence returns total size
+    cout << "sizeof(&arr): " << sizeof(&arr) << endl; // size of pointer
+    cout << "sizeof(arr[0]): " << sizeof(arr[0]) << endl;
+    cout << "sizeof(p): " << sizeof(p) << endl;
     return 0;
 }
