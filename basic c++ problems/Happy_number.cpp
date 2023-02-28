b9c421e0282bc1d9e7b42e579811ba8e56f0b181
sasidharan10 <ksasidharan98@gmail.com>
2023-03-01 01:54:31 +0530

    c

--- basic c++ problems/Happy_number.cpp
+++ basic c++ problems/Happy_number.cpp
@@ -1,37 +1,37 @@
-#include<iostream>
-#include<stdio.h>
+#include <iostream>
+#include <stdio.h>
 using namespace std;
 
-//isHappyNumber() will determine whether a number is happy or not  
+// isHappyNumber() will determine whether a number is happy or not
 int isHappy(int result)
 {
-    //Calculates the sum of squares of digits 
-    int sum=0,rem=0;
-    while(result>0)
+    // Calculates the sum of squares of digits
+    int sum = 0, rem = 0;
+    while (result > 0)
     {
-        rem=result%10;
-        sum=sum+(rem*rem);
-        result=result/10;
+        rem = result % 10;
+        sum = sum + (rem * rem);
+        result = result / 10;
     }
     return sum;
 }
 int main()
 {
-    int num,result;
-    cout<<"Enter a number : ";
-    cin>>num;
-    result=num;
+    int num, result;
+    cout << "Enter a number : ";
+    cin >> num;
+    result = num;
 
-    while(result!=1 && result !=4)
-        result=isHappy(result);
+    while (result != 1 && result != 4)
+        result = isHappy(result);
 
-    //Happy number always ends with 1  
-    if(result==1)
-        cout<<num<<" is a Happy number";
+    // Happy number always ends with 1
+    if (result == 1)
+        cout << num << " is a Happy number";
 
-    //Unhappy number ends in a cycle of repeating numbers which contains 4
-    if(result==4)
-        cout<<num<<" is a Not Happy numer";
+    // Unhappy number ends in a cycle of repeating numbers which contains 4
+    if (result == 4)
+        cout << num << " is a Not Happy numer";
     return 0;
 }
 
