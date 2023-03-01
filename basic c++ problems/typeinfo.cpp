8f5a4f938bf8c106296520ee9ef757d37f1a6d65
sasidharan10 <ksasidharan98@gmail.com>
2023-03-02 01:16:12 +0530

    c

--- basic c++ problems/typeinfo.cpp
+++ basic c++ problems/typeinfo.cpp
@@ -8,10 +8,10 @@ int main()
     char c = 'z';
     double d = 126;
     long long int e = 126;
-    cout<<typeid(a).name()<<endl;
-    cout<<typeid(b).name()<<endl;
-    cout<<typeid(c).name()<<endl;
-    cout<<typeid(d).name()<<endl;
-    cout<<typeid(e).name()<<endl;
+    cout << typeid(a).name() << endl;
+    cout << typeid(b).name() << endl;
+    cout << typeid(c).name() << endl;
+    cout << typeid(d).name() << endl;
+    cout << typeid(e).name() << endl;
     return 0;
 }
