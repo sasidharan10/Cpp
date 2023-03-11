32177e0de133d29b7d71347c545d4845e5515270
sasidharan10 <ksasidharan98@gmail.com>
2023-03-11 23:13:41 +0530

    c

--- basic c++ problems/1_practice.cpp
+++ basic c++ problems/1_practice.cpp
@@ -15,7 +15,7 @@ int recur(int *arr, int beg, int end, int x)
     }
     return -1;
 }
-int binary_search(int *arr, int n, int x)
+int Binary_search(int *arr, int n, int x)
 {
     int mid = 0, beg = 0, end = n - 1;
     return recur(arr, 0, end, x);
@@ -24,8 +24,8 @@ int main()
 {
     int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     int n = sizeof(arr) / sizeof(arr[0]);
-    int x = 7;
-    int ans = binary_search(arr, n, x);
+    int x = 8;
+    int ans = Binary_search(arr, n, x);
     // int ans=recur(arr,0,n-1,x);
     if (ans != -1)
         cout << "Found at index: " << ans << endl;
