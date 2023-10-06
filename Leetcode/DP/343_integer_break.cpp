bd6934e503012cefa93c02c49ed960b7ff3812be
sasidharan10 <ksasidharan98@gmail.com>
2023-10-06 21:21:13 +0530

    c

---
+++ Leetcode/DP/343_integer_break.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    // Recursion
+    int integerBreakRecur(int n)
+    {
+        // TC: O(n^n-1), exponential
+        // SC: O(n * n - 1)
+
+        if (n == 1)
+            return 1;
+        int res = INT_MIN;
+        for (int i = 1; i < n; i++)
+        {
+            int prod = i * max(n - i, integerBreakRecur(n - i));
+            res = max(res, prod);
+        }
+        return res;
+    }
+    // Memoization
+    int integerBreakMem(int n, vector<int> &dp)
+    {
+        if (n == 1)
+            return 1;
+        if (dp[n] != -1)
+            return dp[n];
+        int res = INT_MIN;
+        for (int i = 1; i < n; i++)
+        {
+            int prod = i * max(n - i, integerBreakMem(n - i, dp));
+            res = max(res, prod);
+        }
+        return dp[n] = res;
+    }
+    // Tabulation
+
+    // Space Optimization
+
+    int integerBreak(int n)
+    {
+        // return integerBreakRecur(n);
+        vector<int> dp(n + 1, -1);
+        return integerBreakMem(n, dp);
+    }
+};
+int main()
+{
+    Solution s;
+    int n = 10;
+    cout << "Max Product by dividing into k elements: " << s.integerBreak(n);
+    return 0;
+}
+
+/*
+
+link:
+
+leetcode: https://leetcode.com/problems/integer-break/?envType=daily-question&envId=2023-10-06
+
+Youtube: https://www.youtube.com/watch?v=iXtqfIrWMZg
+
+algorithm:
+
+*/
