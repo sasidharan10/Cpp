335b4f2c1c889abaaa15bfdc36ff99438545c042
sasidharan10 <ksasidharan98@gmail.com>
2023-10-08 19:31:53 +0530

    c

--- Leetcode/DP/1458_mar_prod_sum_seq.cpp
+++ Leetcode/DP/1458_mar_prod_sum_seq.cpp
@@ -6,9 +6,9 @@ public:
     // Recursion
     int maxDotProductRecur(int ind1, int ind2, vector<int> &nums1, vector<int> &nums2)
     {
-        if (ind1 == -1 || ind2 == -1)
+        if (ind1 == 0 || ind2 == 0)
             return 0;
-        int r1 = (nums1[ind1] * nums2[ind2]) + maxDotProductRecur(ind1 - 1, ind2 - 1, nums1, nums2);
+        int r1 = (nums1[ind1 - 1] * nums2[ind2 - 1]) + maxDotProductRecur(ind1 - 1, ind2 - 1, nums1, nums2);
         return max({r1, maxDotProductRecur(ind1 - 1, ind2, nums1, nums2), maxDotProductRecur(ind1, ind2 - 1, nums1, nums2)});
     }
     // Memoization
@@ -22,9 +22,38 @@ public:
         return dp[ind1][ind2] = max({r1, maxDotProductMem(ind1 - 1, ind2, nums1, nums2, dp), maxDotProductMem(ind1, ind2 - 1, nums1, nums2, dp)});
     }
     // Tabulation
-
+    int maxDotProductTab(vector<int> &nums1, vector<int> &nums2)
+    {
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
+        for (int ind1 = 1; ind1 <= n1; ind1++)
+        {
+            for (int ind2 = 1; ind2 <= n2; ind2++)
+            {
+                int r1 = (nums1[ind1 - 1] * nums2[ind2 - 1]) + dp[ind1 - 1][ind2 - 1];
+                dp[ind1][ind2] = max({r1, dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]});
+            }
+        }
+        return dp[n1][n2];
+    }
     // Space Optimization
-
+    int maxDotProductSpc(vector<int> &nums1, vector<int> &nums2)
+    {
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        vector<int> prev(n2 + 1, 0), cur(n2 + 1, 0);
+        for (int ind1 = 1; ind1 <= n1; ind1++)
+        {
+            for (int ind2 = 1; ind2 <= n2; ind2++)
+            {
+                int r1 = (nums1[ind1 - 1] * nums2[ind2 - 1]) + prev[ind2 - 1];
+                cur[ind2] = max({r1, prev[ind2], cur[ind2 - 1]});
+            }
+            prev = cur;
+        }
+        return cur[n2];
+    }
     int maxDotProduct(vector<int> &nums1, vector<int> &nums2)
     {
         int n1 = nums1.size();
@@ -53,9 +82,11 @@ public:
         {
             return max(firstMax * secondMin, firstMin * secondMax);
         }
-        // return maxDotProductRecur(n1 - 1, n2 - 1, nums1, nums2);
+        // return maxDotProductRecur(n1, n2, nums1, nums2);
         vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, INT_MIN));
-        return maxDotProductMem(n1, n2, nums1, nums2, dp);
+        // return maxDotProductMem(n1, n2, nums1, nums2, dp);
+        // return maxDotProductTab(nums1, nums2);
+        return maxDotProductSpc(nums1, nums2);
     }
 };
 int main()
@@ -69,12 +100,12 @@ int main()
 
 /*
 
-link: 
+link:
 
 leetcode: https://leetcode.com/problems/max-dot-product-of-two-subsequences/description/
 
-Youtube: 
+Youtube:
 
-algorithm: 
+algorithm:
 
 */
