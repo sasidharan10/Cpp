b1a4424d4989f764c518eba33c3f049a846b22fe
sasidharan10 <ksasidharan98@gmail.com>
2023-12-15 14:31:50 +0530

    monotonic stack

--- Leetcode/Monotonic Stack/739_daily_temperatures.cpp
+++ Leetcode/Monotonic Stack/739_daily_temperatures.cpp
@@ -3,8 +3,56 @@ using namespace std;
 class Solution
 {
 public:
+    // Brute
+    vector<int> dailyTemperatures2(vector<int> &temperatures)
+    {
+        // TC: O(n^2)
+        // SC: O(1) or O(n) for storing ans
+
+        int n = temperatures.size();
+        if (n == 1)
+            return {0};
+        int j = 1;
+        vector<int> ans(n, 0);
+        for (int i = 0; i < n; i++)
+        {
+            while (j < n && temperatures[i] >= temperatures[j])
+            {
+                j++;
+            }
+            if (j >= n)
+                ans[i] = 0;
+            else
+                ans[i] = j - i;
+            j = i + 2;
+        }
+        return ans;
+    }
+    
+    // Optimal
     vector<int> dailyTemperatures(vector<int> &temperatures)
     {
+        // TC: O(n)
+        // SC: O(n) * 2
+        
+        int n = temperatures.size();
+        if (n == 1)
+            return {0};
+        stack<int> st;
+        vector<int> ans(n, 0);
+        for (int i = n - 1; i >= 0; i--)
+        {
+            while (!st.empty() && temperatures[i] >= temperatures[st.top()])
+            {
+                st.pop();
+            }
+            if (st.empty())
+                ans[i] = 0;
+            else
+                ans[i] = st.top() - i;
+            st.push(i);
+        }
+        return ans;
     }
 };
 int main()
@@ -29,6 +77,17 @@ leetcode: https://leetcode.com/problems/daily-temperatures
 Youtube: https://www.youtube.com/watch?v=ZhZEmGv-sOY
 
 algorithm:
+
+- We use a monotonic stack(maintains increasing order) and store the indices of
+  temperatures, which maintains a increasing order.
+- We traverse from back, and check if cur element is greater than st.top().
+  If yes, then we pop() elements from stack, until we find a greater
+  element (i.e., st.top() > cur).
+- If found, that means these is a greater element present in array, and its
+  distance from cur element => (st.top() - cur_index).
+- If no element fund, stack becomes empty. hence there is no element greater than
+  cur element, hence we store 0.
+- Once the check is done, we push the current element in stack and continue.
 
 */
 
