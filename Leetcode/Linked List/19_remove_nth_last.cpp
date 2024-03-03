5e9e259c49d3ff81673677a609d177ad06b4d46a
sasidharan10 <ksasidharan98@gmail.com>
2024-03-03 15:49:40 +0530

    dc

--- Leetcode/Linked List/19_remove_nth_last.cpp
+++ Leetcode/Linked List/19_remove_nth_last.cpp
@@ -96,7 +96,7 @@ int main()
     head = s.InsertEnd(head, 4);
     head = s.InsertEnd(head, 5);
     s.printList(head);
-    head = s.removeNthFromEnd(head, 1);
+    head = s.removeNthFromEnd(head, 3);
     s.printList(head);
     return 0;
 }
@@ -106,5 +106,49 @@ int main()
 link:
 
 leetcode: https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+
+*/
+
+/*
+
+19. Remove Nth Node From End of List
+
+Given the head of a linked list, remove the nth node from the end 
+of the list and return its head.
+
+Example 1:
+
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+
+Example 2:
+
+Input: head = [1], n = 1
+Output: []
+
+Example 3:
+
+Input: head = [1,2], n = 1
+Output: [1]
+
+*/
+
+/*
+
+public static ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
 
 */
