a88934b2c22051be569a52fbf66bc259a91edd2f
sasidharan10 <ksasidharan98@gmail.com>
2024-03-21 12:53:02 +0530

    lc 206

--- Leetcode/Linked List/206_reverse_LL.cpp
+++ Leetcode/Linked List/206_reverse_LL.cpp
@@ -73,3 +73,59 @@ int main()
     s.printList(head);
     return 0;
 }
+
+/*
+
+link: 
+
+leetcode: https://leetcode.com/problems/reverse-linked-list/
+
+Youtube: 
+
+algorithm: 
+
+Code Link: 
+
+*/
+
+/*
+
+206. Reverse Linked List
+
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Example 1:
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Example 2:
+
+Input: head = [1,2]
+Output: [2,1]
+
+Example 3:
+
+Input: head = []
+Output: []
+
+*/
+
+
+/*
+
+public static ListNode reverseList(ListNode head) {
+        ListNode current = head;
+        ListNode nextNode = null;
+        ListNode prev = null;
+        while (current != null) {
+            nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+        head = prev;
+        return head;
+    }
+
+*/
