9c26c0b857344ce4510f5f9d264a0ac6306c4837
sasidharan10 <ksasidharan98@gmail.com>
2024-03-22 17:02:19 +0530

    lc 234

--- Leetcode/Linked List/234_check_palindrome.cpp
+++ Leetcode/Linked List/234_check_palindrome.cpp
@@ -34,12 +34,13 @@ public:
     void print(ListNode *p)
     {
 
-        cout << "\nList: ";
+        cout << "List: ";
         while (p != NULL)
         {
             cout << p->val << " ";
             p = p->next;
         }
+        cout << endl;
     }
     ListNode *ReverseList(ListNode *head)
     {
@@ -57,8 +58,12 @@ public:
         }
         return prev;
     }
-    bool isPalindrome(ListNode *head)
+    // Better - by revering the half list, and checking both halfs, if they are same
+    bool isPalindrome3(ListNode *head)
     {
+        // TC: O(n) + O(n)
+        // SC: O(1)
+
         ListNode *low = head;
         ListNode *high = head->next;
         while (high != NULL && high->next != NULL)
@@ -81,6 +86,56 @@ public:
         }
         return true;
     }
+
+    // Optimal - We reverse the half list, while finding the mid, then check both halfs
+    bool isPalindrome2(ListNode *head)
+    {
+        // TC: O(n)
+        // SC: O(1)
+
+        ListNode *low = head;
+        ListNode *high = head;
+        ListNode *prev = nullptr;
+        ListNode *nextNode = nullptr;
+        while (high != NULL && high->next != NULL)
+        {
+            high = high->next->next;
+            nextNode = low->next;
+            low->next = prev;
+            prev = low;
+            low = nextNode;
+        }
+
+        // incase of odd elements, low will be pointing at the mid element if
+        // high!= nullptr, hence we move 1 node further, to check with remaining elements
+        if (high != nullptr)
+            low = low->next;
+        while (low && prev)
+        {
+            if (low->val != prev->val)
+                return false;
+            low = low->next;
+            prev = prev->next;
+        }
+        return true;
+    }
+
+    // Optimal - Using recursion, we go till tail, then check the tail->val == front->val
+    ListNode *front;   // global variable
+    bool isPalindromeUtil(ListNode *tail)
+    {
+        bool res = true;
+        if (tail->next)
+            res = isPalindromeUtil(tail->next);
+        res = res & (front->val == tail->val);
+        front = front->next;
+        return res;
+    }
+    bool isPalindrome(ListNode *head)
+    {
+        front = head;
+        return isPalindromeUtil(head);
+    }
 };
 int main()
 {
@@ -93,6 +148,7 @@ int main()
     head = s.InsertEnd(head, 1);
     head = s.InsertEnd(head, 2);
     head = s.InsertEnd(head, 3);
+    // head = s.InsertEnd(head, 3);
     s.print(head);
     cout << "Palindrome? : " << s.isPalindrome(head);
     return 0;
@@ -103,5 +159,46 @@ int main()
 link:
 
 leetcode: https://leetcode.com/problems/palindrome-linked-list/description/
+
+youtube: https://www.youtube.com/watch?v=Sgi2BHiW0-Q
+
+*/
+
+/*
+
+234. Palindrome Linked List
+
+Given the head of a singly linked list, return true if it is a 
+palindrome or false otherwise.
+
+Example 1:
+
+Input: head = [1,2,2,1]
+Output: true
+
+Example 2:
+
+Input: head = [1,2]
+Output: false
+
+*/
+
+/*
+
+    public static ListNode front; // global variable
+
+    public static boolean isPalindromeUtil(ListNode tail) {
+        boolean res = true;
+        if (tail.next != null)
+            res = isPalindromeUtil(tail.next);
+        res = res & (front.val == tail.val);
+        front = front.next;
+        return res;
+    }
+
+    public static boolean isPalindrome(ListNode head) {
+        front = head;
+        return isPalindromeUtil(head);
+    }
 
 */
