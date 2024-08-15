e96141e5305d60cada435ace442dcded8b351433
sasidharan10 <ksasidharan98@gmail.com>
2024-08-15 18:12:47 +0530

    lc 860

--- Leetcode/Graph/1568_min_days_disconnect.cpp
+++ Leetcode/Graph/1568_min_days_disconnect.cpp
@@ -96,20 +96,57 @@ algorithm:
 
 - Brute Force Approach:
 
-- 
+- As per the given problem, We should not have exactly 1 island. Either we can have 0 island or
+  more than 1 islands. If we have exactly 1 island, then we have to disconnect the island into
+  2 parts.
+- To disconnect an island, we can change one grid of land into water per day.
+- Hence we need t find the minimum no of days to disconnect the island.
+- Note that, no matter how big the island is, we only need to change, at max, 2 grids, to disconnect the 
+  any given island.
+- So, first we check the no of islands in the given matrix. If its 0, or more 1, then return 0.
+- If exactly 1 island exists, then we need to disconnect it.
+- Now we apply brute force, to check, if we can disconnect an island, by change 1 grid.
+- We try to change every single grid into 0, then apply BFS, or DFS, to find the no of islands
+  after the change. If the island gets disconnected ,then we return 1.
+- If its not poosible by changing 1 grid, then surely we can disconnect it using 2 grids, 
+  hence we just return 2.
 
 - Optimal Approach:
 
-- self explanatory
+- Using tarjams algorithm.
 
 */
 
 /*
 
+1568. Minimum Number of Days to Disconnect Island
 
+You are given an m x n binary grid grid where 1 represents land and 0 represents water. An 
+island is a maximal 4-directionally (horizontal or vertical) connected group of 1's.
+The grid is said to be connected if we have exactly one island, otherwise is said disconnected.
+In one day, we are allowed to change any single land cell (1) into a water cell (0).
+Return the minimum number of days to disconnect the grid.
 
+Example 1:
 
+Input: grid = [[0,1,1,0],[0,1,1,0],[0,0,0,0]]
 
+Output: 2
+Explanation: We need at least 2 days to get a disconnected grid.
+Change land grid[1][1] and grid[0][2] to water and get 2 disconnected island.
+
+Example 2:
+
+Input: grid = [[1,1]]
+Output: 2
+Explanation: Grid of full water is also disconnected ([[1,1]] -> [[0,0]]), 0 islands.
+ 
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 30
+grid[i][j] is either 0 or 1.
 
 */
 
---
+++ Leetcode/Heap/719_kth_sml_pair.cpp
@@ -0,0 +1,156 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    // Brute force - Gives TLE
+    int smallestDistancePair2(vector<int> &nums, int k)
+    {
+        // TC: O(n^2 * logk)
+        // SC: O(k)
+
+        int n = nums.size();
+        priority_queue<int> pq; // max-heap
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                int diff = abs(nums[i] - nums[j]);
+                if (pq.empty() || diff < pq.top())
+                    pq.push(diff);
+                if (pq.size() > k)
+                    pq.pop();
+            }
+        }
+        return pq.top();
+    }
+
+    // Using map
+    int smallestDistancePair(vector<int> &nums, int k)
+    {
+        // TC: O(n^2 * logk)
+        // SC: O(k)
+
+        int n = nums.size();
+        priority_queue<int, vector<int>, greater<>> pq;
+        unordered_map<int, int> mp;
+        for (int i = 0; i < n; i++)
+        {
+            mp[nums[i]]++;
+        }
+        for (auto &&i : mp)
+        {
+            for (auto &&j : mp)
+            {
+                int num1 = i.first;
+                int num2 = j.first;
+                int freq1 = i.second;
+                int freq2 = j.second;
+                if (num1 == num2)
+                    continue;
+                if (freq1 > 1)
+                {
+                    int temp = (freq1 * (freq1 - 1)) / 2;
+                    for (int k = 0; k < temp; k++)
+                    {
+                        pq.push(0);
+                    }
+                }
+                if (freq2 > 1)
+                {
+                    int temp = (freq2 * (freq2 - 1)) / 2;
+                    for (int k = 0; k < temp; k++)
+                    {
+                        pq.push(0);
+                    }
+                }
+                int diff = abs(num1 - num2);
+                int temp = freq1 * freq2;
+                for (int k = 0; k < temp; k++)
+                {
+                    pq.push(diff);
+                }
+            }
+        }
+        while (k > 1)
+        {
+            pq.pop();
+            k--;
+        }
+        return pq.top();
+    }
+};
+int main()
+{
+    Solution s;
+    vector<int> nums{1, 6, 1};
+    int k = 3;
+    cout << "Result: " << s.smallestDistancePair(nums, k) << endl;
+    return 0;
+}
+
+/*
+
+link:
+
+leetcode:
+
+Youtube:
+
+Code Link:
+
+algorithm:
+
+- Brute Force Approach:
+
+-
+
+- Optimal Approach:
+
+- self explanatory
+
+*/
+
+/*
+
+719. Find K-th Smallest Pair Distance
+
+The distance of a pair of integers a and b is defined as the absolute difference between a and b.
+Given an integer array nums and an integer k, return the kth smallest distance among all the
+pairs nums[i] and nums[j] where 0 <= i < j < nums.length.
+
+Example 1:
+
+Input: nums = [1,3,1], k = 1
+Output: 0
+Explanation: Here are all the pairs:
+(1,3) -> 2
+(1,1) -> 0
+(3,1) -> 2
+Then the 1st smallest distance pair is (1,1), and its distance is 0.
+
+Example 2:
+
+Input: nums = [1,1,1], k = 2
+Output: 0
+
+Example 3:
+
+Input: nums = [1,6,1], k = 3
+Output: 5
+
+Constraints:
+
+n == nums.length
+2 <= n <= 104
+0 <= nums[i] <= 106
+1 <= k <= n * (n - 1) / 2
+
+*/
+
+/*
+************* Java Code **************
+
+
+
+*/
